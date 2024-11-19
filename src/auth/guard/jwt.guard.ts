553219a130a74c32bfdfcf48b4822943a3d2bab2
Hồng Nhật Huỳnh Trần <nhatcomnhom@gmail.com>
2024-11-19 22:34:13 +0900

    add UnauthorizedException message

--- src/auth/guard/jwt.guard.ts
+++ src/auth/guard/jwt.guard.ts
@@ -1,15 +1,17 @@
-import { Injectable, UnauthorizedException } from '@nestjs/common';
+import { ExecutionContext, Injectable, UnauthorizedException } from '@nestjs/common';
 import { AuthGuard } from '@nestjs/passport';
+import { Observable } from 'rxjs';
 
 @Injectable()
 export class JwtGuard extends AuthGuard('jwt') {
   constructor() {
     super();
   }
-  // handleRequest(err, user, info) {
-  //   if (err || !user) {
-  //     throw err || new UnauthorizedException();
-  //   }
-  //   return user;
-  // }
+  
+  handleRequest(err, user, info) {
+    if (err || !user) {
+      throw err || new UnauthorizedException("Access Token not exist!");
+    }
+    return user;
+  }
 }
